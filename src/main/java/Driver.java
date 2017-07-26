ebce202d828b8330b060d77c773f35c92e579a68
MinBigData <66909573@qq.com>
2017-07-25 21:00:34 -0700

    add comments to driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -24,6 +24,8 @@ public class Driver {
 
 		//job1
 		Configuration conf1 = new Configuration();
+
+		//Define the job to read data sentence by sentence
 		conf1.set("textinputformat.record.delimiter", ".");
 		conf1.set("noGram", numberOfNGram);
 		
@@ -51,7 +53,8 @@ public class Driver {
 		Configuration conf2 = new Configuration();
 		conf2.set("threshold", threshold);
 		conf2.set("n", numberOfFollowingWords);
-		
+
+		//Use dbConfiguration to configure all the jdbcDriver, db user, db password, database
 		DBConfiguration.configureDB(conf2, 
 				"com.mysql.jdbc.Driver",
 				"jdbc:mysql://ip_address:port/test",
@@ -61,8 +64,16 @@ public class Driver {
 		Job job2 = Job.getInstance(conf2);
 		job2.setJobName("Model");
 		job2.setJarByClass(Driver.class);
-		
+
+		//How to add external dependency to current project?
+		/*
+		  1. upload dependency to hdfs
+		  2. use this "addArchiveToClassPath" method to define the dependency path on hdfs
+		 */
 		job2.addArchiveToClassPath(new Path("path_to_ur_connector"));
+
+		//Why do we add map outputKey and outputValue?
+		//Because map output key and value are inconsistent with reducer output key and value
 		job2.setMapOutputKeyClass(Text.class);
 		job2.setMapOutputValueClass(Text.class);
 		job2.setOutputKeyClass(DBOutputWritable.class);
@@ -73,7 +84,8 @@ public class Driver {
 		
 		job2.setInputFormatClass(TextInputFormat.class);
 		job2.setOutputFormatClass(DBOutputFormat.class);
-		
+
+		//use dbOutputformat to define the table name and columns
 		DBOutputFormat.setOutput(job2, "output", 
 				new String[] {"starting_phrase", "following_word", "count"});
 
